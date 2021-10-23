53cacf194387d68b425f153917b41a46484db20a
Esteban <estebansalazar498@gmail.com>
2021-10-22 19:16:03 -0500

    :sparkless: Add validations

--- app/models/project_detail.rb
+++ app/models/project_detail.rb
@@ -1,4 +1,23 @@
 class ProjectDetail < ApplicationRecord
+
+  validates :name, presence: true, uniqueness: true, format: {with: /[
+    a-zA-Z0-9_-]{2}/,message: "must contain name"}
+
+  validates :video_url, presence: true, uniqueness: true, format: {with: /[
+    a-zA-Z0-9_-]{2}/,message: "must contain video url"}
+
+  validates :category, presence: true, uniqueness: false, format: {with: /[
+    a-zA-Z0-9_-]{2}/,message: "must contain category field"}
+
+  validates :area, presence: true, uniqueness: false, format: {with: /[
+    a-zA-Z0-9_-]{2}/,message: "must contain area"}
+
+  validates :client_type, presence: true, uniqueness: false, format: {with: /[
+    a-zA-Z0-9_-]{2}/,message: "must contain client type"}
+
+  validates :description, presence: true, uniqueness: false, format: {with: /[
+    a-zA-Z0-9_-]{2}/,message: "must contain description"}
+
   belongs_to :project
   has_one :project_category
   accepts_nested_attributes_for :project_category
--- app/models/social_impact.rb
+++ app/models/social_impact.rb
@@ -1,3 +1,7 @@
 class SocialImpact < ApplicationRecord
+
+
+
+
   belongs_to :project
 end
