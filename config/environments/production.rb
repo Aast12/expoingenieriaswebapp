3290c3176c3f23bdb0c3909b516d276a7193ddca
FerSanchez99 <mafsr1999@gmail.com>
2021-11-15 10:08:28 -0600

    Fixed host in confirmable link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://fathomless-castle-25246.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.office365.com",
