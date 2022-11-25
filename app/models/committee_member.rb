db6d3b53728aae89f81768023b528b64bff9667d
Elias Garza <elias.garza@kalypso.com>
2022-11-25 02:20:56 -0600

    clean gitignore

--- app/models/committee_member.rb
+++ app/models/committee_member.rb
@@ -9,4 +9,7 @@ class CommitteeMember < ApplicationRecord
         User.find(self.user_id)
     end
 
+    def professor_or_judge_or_committee_member? 
+        self.get_user.professor? or self.get_user.judge? or self.get_user.committee_member?
+    end
 end
