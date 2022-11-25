bf19268543e4e65349b5ac1ec47f74cbdea58b2f
Elias Garza <elias.garza@kalypso.com>
2022-11-25 02:19:44 -0600

    solve members user access bug & refactor validations

--- app/models/committee_member.rb
+++ app/models/committee_member.rb
@@ -1,3 +1,12 @@
 class CommitteeMember < ApplicationRecord
     has_one :user, as: :userable
+    
+    def has_user?
+        User.first.present?
+    end
+
+    def get_user
+        User.find(self.user_id)
+    end
+
 end
